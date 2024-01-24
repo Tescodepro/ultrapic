de3d9c3181d781a3635db6ddf79779af7ca49903
Tescodepro <tescodepro@gmail.com>
2024-01-24 16:03:44 +0100

    admin and matric update

--- controllers/database.php
+++ controllers/database.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost"; 
-$username = "root";
-$password = "";
-$database = "ultrapic";
-
 // $servername = "localhost"; 
-// $username = "ultrapic";
-// $password = "Ultrapic0o2023";
+// $username = "root";
+// $password = "";
 // $database = "ultrapic";
+
+$servername = "localhost"; 
+$username = "ultrapic";
+$password = "Ultrapic0o2023";
+$database = "ultrapic";
 
 // Create connection
 $dbconnect = mysqli_connect($servername, $username, $password, $database);
